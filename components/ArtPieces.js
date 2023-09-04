27990e529bb6086683e1d4dc1d16e58b7b5ab7ef
Miriam Ertl <miriam_ertl@web.de>
2023-09-04 10:58:37 +0200

    feat: hand props to ArtPiecePreview

--- components/ArtPieces.js
+++ components/ArtPieces.js
@@ -1,8 +1,16 @@
+import ArtPiecePreview from "./ArtPiecePreview";
+
 export default function ArtPieces({ pieces }) {
   return (
     <ul>
       {pieces.map((piece) => (
-        <ArtPiecePreview />
+        <>
+          <ArtPiecePreview
+            image={piece.imageSource}
+            title={piece.name}
+            artist={piece.artist}
+          />
+        </>
       ))}
     </ul>
   );
