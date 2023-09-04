09a082e66d530bdf77fe9a8b555f6c479eaed5ba
Bonnie Baumeister <bonnie.hoika@gmx.de>
2023-09-04 14:33:48 +0200

    feat: moving random function to index file

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,15 @@
 import GlobalStyle from "../styles";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+const { data, isLoading } = useSWR(
+  `https://example-apis.vercel.app/api/art`,
+  fetcher
+);
+if (isLoading || !data) {
+  return "Loading ...";
+}
 
 export default function App({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,6 @@
-import useSWR from "swr";
 import Spotlight from "../components/Spotlight";
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
-
 export default function OverviewPage() {
-  const { data, isLoading } = useSWR(
-    `https://example-apis.vercel.app/api/art`,
-    fetcher
-  );
-  if (isLoading || !data) {
-    return "Loading ...";
-  }
-
   const length = data.length;
   console.log(length);
 
