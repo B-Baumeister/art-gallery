c9268e04d3bfa552b0c4db2b2277841f69a735bd
Bonnie Baumeister <bonnie.hoika@gmx.de>
2023-09-04 15:22:31 +0200

    refactor: make pieces available on all pages

--- pages/_app.js
+++ pages/_app.js
@@ -3,19 +3,16 @@ import useSWR from "swr";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-const { data, isLoading } = useSWR(
-  `https://example-apis.vercel.app/api/art`,
-  fetcher
-);
-if (isLoading || !data) {
-  return "Loading ...";
-}
-
 export default function App({ Component, pageProps }) {
+  const { data, isLoading } = useSWR(
+    `https://example-apis.vercel.app/api/art`,
+    fetcher
+  );
+
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} pieces={isLoading || !data ? [] : data} />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,20 @@
 import Spotlight from "../components/Spotlight";
 
-export default function OverviewPage() {
-  const length = data.length;
-  console.log(length);
-
-  function getRandomPiece(length) {
-    return Math.floor(Math.random() * length);
+export default function OverviewPage({ pieces }) {
+  function getRandomPiece() {
+    return Math.floor(Math.random() * pieces.length);
   }
+
+  const randomPiece = pieces[getRandomPiece()];
+
   return (
     <div>
-      <Spotlight
-        image={data[getRandomPiece(length)].imageSource}
-        artist={data[getRandomPiece(length)].artist}
-      />
+      {randomPiece && (
+        <Spotlight
+          image={randomPiece.imageSource}
+          artist={randomPiece.artist}
+        />
+      )}
     </div>
   );
 }
