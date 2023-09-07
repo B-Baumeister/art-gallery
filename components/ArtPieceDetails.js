5207e520424bc9793f1efbd68ebc661cddc32a5c
Bonnie Baumeister <bonnie.hoika@gmx.de>
2023-09-07 09:00:15 +0200

    feat:add [slug].js and useRouter

--- components/ArtPieceDetails.js
+++ components/ArtPieceDetails.js
@@ -1,16 +1,20 @@
 import Image from "next/image";
 
+// Create the component ArtPieceDetails
+//  ArtPieceDetails props: image, title, artist, year, genre
+
 export default function ArtPieceDetails({ image, title, artist, year, genre }) {
   return (
-    <ul>
-      {pieces.map(({ id, image, title, artist, year, genre }) => (
-        <li key={id}>
-          <Image src={image} alt={artist} width={`${300}`} height={`${300}`} />
-          <p>
-            ”{title}” by {artist}, Year: {year}, Genre: {genre},
-          </p>
+    <>
+      <Image src={image} alt={artist} width={`${300}`} height={`${300}`} />
+
+      <ul>
+        <li>
+          {title} by: {artist}
         </li>
-      ))}
-    </ul>
+        <li>Year: {year}</li>
+        <li> Genre: {genre}</li>
+      </ul>
+    </>
   );
 }
--- pages/art-pieces/[slug].js
+++ pages/art-pieces/[slug].js
@@ -1,8 +1,26 @@
 import ArtPieceDetails from "@/components/ArtPieceDetails";
 import { useRouter } from "next/router";
+
 export default function ArtPieceDetailsPage({ pieces }) {
   const router = useRouter();
-  const { id } = router.query;
+  const { slug } = router.query;
 
-  return <ArtPieceDetails pieces={pieces} />;
+  const selectedArtPiece = pieces.find((piece) => piece.slug === slug);
+
+  return (
+    <>
+      <ArtPieceDetails
+        image={selectedArtPiece.imageSource}
+        title={selectedArtPiece.title}
+        artist={selectedArtPiece.artist}
+        year={selectedArtPiece.year}
+        genre={selectedArtPiece.genre}
+      />
+      ;
+    </>
+  );
 }
+
+// Create the page pages/art-pieces/[slug] that renders ArtPieceDetails
+//  Read the query parameter slug from next/router
+//  Use the slug to find the art piece to display
