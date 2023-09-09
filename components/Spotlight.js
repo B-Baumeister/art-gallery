87fc3cb965b6923c1c6482b891ee881a2aca5430
Miriam Ertl <miriam_ertl@web.de>
2023-09-09 18:45:40 +0200

    feat: render FavoriteButton on Sporlight.js

--- components/Spotlight.js
+++ components/Spotlight.js
@@ -1,9 +1,20 @@
+import FavoriteButton from "./FavoriteButton";
 import Image from "next/image";
 
-export default function Spotlight({ image, artist }) {
+export default function Spotlight({
+  image,
+  artist,
+  isFavorite,
+  onToggleFavorite,
+}) {
   return (
     <div>
       {" "}
+      <FavoriteButton
+        isFavorite={isFavorite}
+        onToggleFavorite={onToggleFavorite}
+        positionAbsolute={true}
+      />
       <Image src={image} alt={artist} width={`${300}`} height={`${300}`} />
       <p>{artist}</p>
     </div>
